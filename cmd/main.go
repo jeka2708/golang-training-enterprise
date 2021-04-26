1a1fa4b5541258a0eb28fbec9195c496c9a27ac9
jeka2708 <jeka_cool_man_2@mail.ru>
2021-04-26 13:16:30 +0300

    add api and front part

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"github.com/jeka2708/golang-training-enterprise/pkg/data"
-	"github.com/jeka2708/golang-training-enterprise/pkg/db"
-	"github.com/jeka2708/golang-training-enterprise2/pkg/api"
 	"log"
 	"net"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jeka2708/golang-training-enterprise/pkg/api"
+	"github.com/jeka2708/golang-training-enterprise/pkg/data"
+	"github.com/jeka2708/golang-training-enterprise/pkg/db"
 )
 
 var (
@@ -24,19 +25,19 @@ func main() {
 	if error != nil {
 		log.Println(error)
 	}
-	//r := mux.NewRouter()
-	//enterprise := data.NewDataEnterprise(conn)
-	//// 4. send enterprise layer to api layer
-	//api.ServeUserResource(r, *enterprise)
-	//// 5. cors for making requests from any domain
-	//r.Use(mux.CORSMethodMiddleware(r))
-	//r.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("./web/"))))
-	//// 6. start server
-	//listener, err := net.Listen("tcp", ":8080")
-	//if err != nil {
-	//	log.Fatal("Server Listen port...")
-	//}
-	//if err := http.Serve(listener, r); err != nil {
-	//	log.Fatal("Server has been crashed...")
-	//}
+	r := mux.NewRouter()
+	enterprise := data.NewDataEnterprise(conn)
+	// 4. send enterprise layer to api layer
+	api.ServeUserResource(r, *enterprise)
+	// 5. cors for making requests from any domain
+	r.Use(mux.CORSMethodMiddleware(r))
+	r.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("./web/"))))
+	// 6. start server
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("Server Listen port...")
+	}
+	if err := http.Serve(listener, r); err != nil {
+		log.Fatal("Server has been crashed...")
+	}
 }
--- pkg/data/worker_test.go
+++ pkg/data/worker_test.go
@@ -3,13 +3,14 @@ package data
 import (
 	"database/sql"
 	"errors"
+	"regexp"
+	"testing"
+
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"regexp"
-	"testing"
 )
 
 type SuiteWorker struct {
